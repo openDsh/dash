b394373f98fdd433e42bad8214c04cc588f35603
Rhys_M <rhys1802@hotmail.co.uk>
2020-07-26 21:35:37 +0100

    Updated install script to work with openDsh add arguments to choose what part of the script will run * no options will run a full install of all of the below * --deps - will install dependencies * --aasdk - will install aasdk * --gstreamer - will install gstreamer * --openauto - will install open auto * --dash - will install dash

--- install.sh
+++ install.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
+echo "Running apt update"
+
+#set default arguments
+deps=false
+aasdk=false
+gstreamer=false
+openauto=false
+dash=false
+perms=false
+
+if [ $# -gt 0 ]; then
+    while [ "$1" != "" ]; do
+        case $1 in
+            --deps )           shift
+                                    deps=true
+                                    ;;
+            --aasdk )           aasdk=true
+                                    ;;
+            --gstreamer )       gstreamer=true
+                                    ;;
+            --openauto )       openauto=true
+                                    ;;
+            --dash )           dash=true
+                                    ;;
+            --perms )          perms=true
+                                    ;;
+            -h | --help )           usage
+                                    exit
+                                    ;;
+            * )                     usage
+                                    exit 1
+        esac
+        shift
+    done
+
+else
+    echo "Your command line contains no arguments"
+    deps=true
+    aasdk=true
+    gstreamer=true
+    openauto=true
+    dash=true
+    perms=true
+fi
+
+
 
 #Array of dependencies any new dependencies can be added here
-deps=(
+dependencies=(
 "alsa-utils"
 "cmake"
 "libboost-all-dev"
@@ -22,86 +68,340 @@ deps=(
 "librtaudio6"
 "libkf5bluezqt-dev"
 "libtag1-dev"
+"qml-module-qtquick2"
+"doxygen"
+"qml-module-qtquick*"
+"libglib2.0-dev"
+"libgstreamer1.0-dev"
+"gstreamer1.0-plugins-base-apps"
+"gstreamer1.0-plugins-bad"
+"gstreamer1.0-libav"
+"gstreamer1.0-alsa"
+"libgstreamer-plugins-base1.0-dev"
+"qtdeclarative5-dev"
+"qt5-default"
+"libgstreamer-plugins-bad1.0-dev"
+"libunwind-dev"
+"qml-module-qtmultimedia"
 )
 
-#loop through dependencies and install
-for app in ${deps[@]}; do
-	echo "installing: " $app
-	sudo apt -qq -o=Dpkg::Use-Pty=0 install $app -y > /dev/null 2> /dev/null
+###############################  dependencies  #########################
+if [ "$deps" = true ]; then
+  #loop through dependencies and install
+	echo "installing dependencies"
+  for app in ${dependencies[@]}; do
+    echo "installing: " $app
+    sudo apt -qq -o=Dpkg::Use-Pty=0 install $app -y > /dev/null 2> /dev/null
 
-	if [[ $? > 0 ]]
-	then
-	    echo $app " Failed to install, quitting"
-	    exit
-	else
-	    echo $app " Installed ok"
-	    echo
-	    
-	fi
-done
-
-echo "All dependencies installed"
-echo
-
-#make ilclient
-echo "making ilclient"
-make /opt/vc/src/hello_pi/libs/ilclient
-if [[ $? > 0 ]]
-  then
-    echo "unable to make ilclient"
-  exit
-else
-  echo "made ok"
+    if [[ $? > 0 ]]
+      then
+          echo $app " Failed to install, quitting"
+          exit
+      else
+          echo $app " Installed ok"
+          echo
+    fi
+  done
+  echo "All dependencies installed"
   echo
-fi
-
-#begin cmake
-echo "beginning cmake for raspberry pi"
-cmake -DRPI_BUILD=TRUE -DGST_BUILD=TRUE -DCMAKE_BUILD_TYPE=Release . 
-if [[ $? > 0 ]]
-  then
-    echo "Cmake error"
-  exit
 else
-  echo "Cmake ok"
-  echo #####
+	echo "skipping dependencies"
 fi
 
-#start make
-echo "beginning make, this will take a while"
-make
-if [[ $? > 0 ]]
-  then
-    echo "make error check output above"
+
+###############################  AASDK #########################
+if [ "$aasdk" = true ]; then
+  #change to parent directory
+  cd ..
+
+  #clone aasdk
+  git clone https://github.com/OpenDsh/aasdk
+  if [[ $? > 0 ]]
+      then
+        cd aasdk
+        if [[ $? > 0 ]]
+          then
+            echo "clone/pull error"
+          exit
+        else
+          git pull https://github.com/OpenDsh/aasdk
+          echo "cloned OK"
+          cd ..
+          echo
+        fi
+  else
+    echo "cloned ok"
+    echo
+  fi
+
+  #change into aasdk folder
+  echo "moving to aasdk"
+  cd aasdk
+
+  #beginning cmake
+  cmake -DCMAKE_BUILD_TYPE=Release .
+  if [[ $? > 0 ]]
+    then
+      echo "CMake error check logs"
     exit
   else
-    echo "make ok, executable can be found ../bin/ia"
+    echo "Cmake ok"
     echo
+  fi
 
-    #check if usb rules exist
-    echo "checking if permissions exist"
+  #beginning make
+  make -j2
 
-    #udev rule to be created below, change as needed
-    FILE=/etc/udev/rules.d/51-iadash.rules
-    if [[ -f "$FILE" ]]
-      then
-        echo "rules exists"
-        echo
-      else
-        sudo touch $FILE
+  if [[ $? > 0 ]]
+    then
+      echo "make error check logs"
+    exit
+  else
+    echo "make ok"
+    echo
+  fi
 
-        # OPEN USB RULE, CREATE MORE SECURE RULE IF REQUIRED
-        echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"*\", ATTR{idProduct}==\"*\", MODE=\"0660\", GROUP=\"plugdev\"" | sudo tee $FILE
-      if [[ $? > 0 ]]
-        then
-          echo "unable to create permissions"
-        else
-          echo "permissions created"
-      fi
-    fi
+  #begin make install
+  sudo make install
+
+  if [[ $? > 0 ]]
+    then
+      echo "install error check logs"
+    exit
+  else
+    echo "installed ok ok"
+    echo
+  fi
+  cd ../dash
+else
+	echo "skipping aasdk"
 fi
 
-#Start app
-echo "starting app"
-cd ../bin
-./ia
+
+###############################  gstreamer  #########################
+#check if gstreamer install is requested
+if [ "$gstreamer" = true ]; then
+  echo "installing gstreamer"
+  #change to parent directory
+  cd ..
+  #clone gstreamer
+  echo "Cloning Gstreamer"
+  git clone git://anongit.freedesktop.org/gstreamer/qt-gstreamer
+  if [[ $? > 0 ]]
+    then
+      cd qt-gstreamer
+      if [[ $? > 0 ]]
+        then
+          echo "clone/pull error"
+        exit
+      else
+        git pull git://anongit.freedesktop.org/gstreamer/qt-gstreamer
+        echo "cloned OK"
+        cd ..
+        echo
+      fi
+  else
+    echo "cloned OK"
+    echo
+  fi
+
+  #change into newly cloned directory
+  cd qt-gstreamer
+
+  #create build directory
+  echo "creating Gstreamer build directory"
+
+  mkdir build
+
+  if [[ $? > 0 ]]
+    then
+      echo "unable to create Gstreamer build directory assuming it exists..."
+  else
+    echo "build directory made"
+    echo
+  fi
+
+  cd build
+
+  #run cmake
+  echo "beginning cmake"
+
+  cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH) -DCMAKE_INSTALL_INCLUDEDIR=include -DQT_VERSION=5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-std=c++11
+
+  if [[ $? > 0 ]]
+    then
+      echo "cmake failed"
+    exit
+  else
+    echo "make ok"
+    echo
+  fi
+
+  #make j4
+  echo "making J4"
+  make -j4
+
+  if [[ $? > 0 ]]
+    then
+      echo "make error check logs"
+    exit
+  else
+    echo "make ok"
+    echo
+  fi
+
+  #run make install
+  echo "beginning make install"
+  sudo make install
+
+  if [[ $? > 0 ]]
+    then
+      echo "install error check logs"
+    exit
+  else
+    echo "installed ok"
+    echo
+  fi
+
+  #run ldconfig
+  sudo ldconfig
+  cd ../../dash
+
+else
+	echo "skipping gstreamer"
+fi
+
+
+
+###############################  openauto  #########################
+if [ "$openauto" = true ]; then
+  echo "installing openauto"
+  cd ..
+
+  clone openauto
+  git clone https://github.com/openDsh/openauto.git
+  if [[ $? > 0 ]]
+    then
+      cd openauto
+      if [[ $? > 0 ]]
+        then
+          echo "clone/pull error"
+        exit
+      else
+        git pull https://github.com/openDsh/openauto.git
+        echo "cloned OK"
+        cd ..
+        echo
+      fi
+  else
+    echo "cloned OK"
+    echo
+  fi
+
+  cd openauto
+
+  #beginning cmake
+  cmake -DRPI_BUILD=true -DGST_BUILD=true
+  if [[ $? > 0 ]]
+    then
+      echo "cmake error check logs"
+    exit
+  else
+    echo "cmake OK"
+    echo
+  fi
+
+  #beginning cmake
+  make
+  if [[ $? > 0 ]]
+    then
+      echo "make error check logs"
+    exit
+  else
+    echo "make OK"
+    echo
+  fi
+
+  #run make install
+  echo "beginning make install"
+  sudo make install
+  if [[ $? > 0 ]]
+    then
+      echo "install error check logs"
+    exit
+  else
+    echo "installed ok"
+    echo
+  fi
+  cd ../dash
+else
+	echo "skipping openauto"
+fi
+
+
+###############################  dash  #########################
+if [ "$dash" = true ]; then
+  #loop through dependencies and install
+	echo "installing dash"
+  echo "making ilclient"
+  make /opt/vc/src/hello_pi/libs/ilclient
+  if [[ $? > 0 ]]
+    then
+      echo "error making ilclient check logs"
+    exit
+  else
+    echo "make ok"
+    echo
+  fi
+
+  echo "running cmake for dash"
+  cmake -DRPI_BUILD=TRUE -DGST_BUILD=TRUE .
+  if [[ $? > 0 ]]
+    then
+      echo "cmake error check logs"
+    exit
+  else
+    echo "cmake OK"
+    echo
+  fi
+
+  echo "running make"
+  make
+  if [[ $? > 0 ]]
+    then
+      echo "make error check output above"
+      exit
+    else
+      echo "make ok, executable can be found ../bin/ia"
+      echo
+
+      #check if usb rules exist
+      echo "checking if permissions exist"
+
+      #udev rule to be created below, change as needed
+      FILE=/etc/udev/rules.d/51-iadash.rules
+      if [[ -f "$FILE" ]]
+        then
+          echo "rules exists"
+          echo
+        else
+          sudo touch $FILE
+
+          # OPEN USB RULE, CREATE MORE SECURE RULE IF REQUIRED
+          echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"*\", ATTR{idProduct}==\"*\", MODE=\"0660\", GROUP=\"plugdev\"" | sudo tee $FILE
+        if [[ $? > 0 ]]
+          then
+            echo "unable to create permissions"
+          else
+            echo "permissions created"
+        fi
+      fi
+  fi
+
+  #Start app
+  echo "starting app"
+  cd bin
+  ./ia
+else
+	echo "skipping dash"
+fi
