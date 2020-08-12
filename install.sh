01c66b930700f976b9bd5d3b834f20d8c17d7b12
rhysmorgan134 <36861722+rhysmorgan134@users.noreply.github.com>
2020-08-13 00:19:10 +0100

    update install script to work with openDsh (#6)
    * Updated install script to work with openDsh
    add arguments to choose what part of the script will run
    * no options will run a full install of all of the below
    * --deps - will install dependencies
    * --aasdk - will install aasdk
    * --gstreamer - will install gstreamer
    * --openauto - will install open auto
    * --dash - will install dash
    
    * Added output for full install
    
    * added help, changed open auto repo to temporary fork
    
    * Updated from PR review
    * Initialise arguments now carried out inside "if" statement
    * Doxygen removed from dependencies
    * qml-module-qtquick* removed from dependencies
    * dependency install now done in one command rather than looping
    * removed all echo quotations
    * blank echos now replaced with ```echo -e xxxxxx '\n'```
    * long "if...else" statements now have shorter condition first
    * removed uneeded quotations
    * cmake -> CMake
    * blank lines removed
    * J4 -> Gstreamer
    * cleaned comments
    * no longer touch file to create, all done from "tee"
    * OS detection and relevant build flag added (to be tested still)
    
    * Boolean to show whether rpi, ilclient installed conditionally
    
    * Added Non Pi support, added raspi-config commands
    
    * Update install.sh
    
    fixed pull for Gstreamer
    
    * changed dash to build inside of build folder
    
    * Replace open auto repo with OpenDsh source
    
    * updated with PR review points, aasdk, openauto now build in their own folder, ilclient removed

--- install.sh
+++ install.sh
@@ -1,7 +1,74 @@
 #!/bin/bash
 
+#repo addresses
+aasdkRepo="https://github.com/OpenDsh/aasdk"
+gstreamerRepo="git://anongit.freedesktop.org/gstreamer/qt-gstreamer"
+openautoRepo="https://github.com/openDsh/openauto"
+
+#Help text
+display_help() {
+    echo
+    echo "   --deps           install all dependencies"
+    echo "   --aasdk          install and build aasdk"
+    echo "   --openauto       install and build openauto "
+    echo "   --gstreamer      install and build gstreamer "
+    echo "   --dash           install and build dash "
+    echo
+    exit 1
+}
+
+#location of OS details for linux
+OS_RELEASE_FILE="/etc/os-release"
+
+#check if Raspian is in the file, if not set the install Args to be false
+if grep -q "Raspbian" ${OS_RELEASE_FILE}; then
+  installArgs="-DRPI_BUILD=true"
+  isRpi=true
+else
+  installArgs=""
+  isRpi=false
+fi
+
+#check to see if there are any arguments supplied, if none are supplied run full install
+if [ $# -gt 0 ]; then
+  #initialize all arguments to false before looping to find which are available
+  deps=false
+  aasdk=false
+  gstreamer=false
+  openauto=false
+  dash=false
+    while [ "$1" != "" ]; do
+        case $1 in
+            --deps )           shift
+                                    deps=true
+                                    ;;
+            --aasdk )           aasdk=true
+                                    ;;
+            --gstreamer )       gstreamer=true
+                                    ;;
+            --openauto )       openauto=true
+                                    ;;
+            --dash )           dash=true
+                                    ;;
+            -h | --help )           display_help
+                                    exit
+                                    ;;
+            * )                     display_help
+                                    exit 1
+        esac
+        shift
+    done
+else
+    echo -e Full install running'\n'
+    deps=true
+    aasdk=true
+    gstreamer=true
+    openauto=true
+    dash=true
+fi
+
 #Array of dependencies any new dependencies can be added here
-deps=(
+dependencies=(
 "alsa-utils"
 "cmake"
 "libboost-all-dev"
@@ -22,86 +89,342 @@ deps=(
 "librtaudio6"
 "libkf5bluezqt-dev"
 "libtag1-dev"
+"qml-module-qtquick2"
+"libglib2.0-dev"
+"libgstreamer1.0-dev"
+"gstreamer1.0-plugins-base-apps"
+"gstreamer1.0-plugins-bad"
+"gstreamer1.0-libav"
+"gstreamer1.0-alsa"
+"libgstreamer-plugins-base1.0-dev"
+"qtdeclarative5-dev"
+"qt5-default"
+"libgstreamer-plugins-bad1.0-dev"
+"libunwind-dev"
+"qml-module-qtmultimedia"
 )
 
-#loop through dependencies and install
-for app in ${deps[@]}; do
-	echo "installing: " $app
-	sudo apt -qq -o=Dpkg::Use-Pty=0 install $app -y > /dev/null 2> /dev/null
-
-	if [[ $? > 0 ]]
-	then
-	    echo $app " Failed to install, quitting"
-	    exit
-	else
-	    echo $app " Installed ok"
-	    echo
-	    
-	fi
-done
-
-echo "All dependencies installed"
-echo
-
-#make ilclient
-echo "making ilclient"
-make /opt/vc/src/hello_pi/libs/ilclient
-if [[ $? > 0 ]]
+###############################  dependencies  #########################
+if [ $deps = false ]
   then
-    echo "unable to make ilclient"
-  exit
-else
-  echo "made ok"
-  echo
-fi
-
-#begin cmake
-echo "beginning cmake for raspberry pi"
-cmake -DRPI_BUILD=TRUE -DGST_BUILD=TRUE -DCMAKE_BUILD_TYPE=Release . 
-if [[ $? > 0 ]]
-  then
-    echo "Cmake error"
-  exit
-else
-  echo "Cmake ok"
-  echo #####
-fi
-
-#start make
-echo "beginning make, this will take a while"
-make
-if [[ $? > 0 ]]
-  then
-    echo "make error check output above"
-    exit
+    echo skipping dependencies '\n'
   else
-    echo "make ok, executable can be found ../bin/ia"
-    echo
+    #loop through dependencies and install
+    echo installing dependencies
+    echo Running apt update
+    sudo apt update
 
-    #check if usb rules exist
-    echo "checking if permissions exist"
+    installString="sudo apt install -y "
 
-    #udev rule to be created below, change as needed
-    FILE=/etc/udev/rules.d/51-iadash.rules
-    if [[ -f "$FILE" ]]
-      then
-        echo "rules exists"
-        echo
-      else
-        sudo touch $FILE
+    #create apt install string
+    for i in ${dependencies[@]}; do
+      installString+=" $i"
+    done
 
-        # OPEN USB RULE, CREATE MORE SECURE RULE IF REQUIRED
-        echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"*\", ATTR{idProduct}==\"*\", MODE=\"0660\", GROUP=\"plugdev\"" | sudo tee $FILE
-      if [[ $? > 0 ]]
-        then
-          echo "unable to create permissions"
-        else
-          echo "permissions created"
-      fi
+    #run install
+    ${installString}
+    if [[ $? -eq 0 ]]; then
+        echo -e All dependencies Installed ok '\n'
+    else
+        echo Package failed to install with error code $?, quitting check logs above
+        exit
     fi
 fi
 
-#Start app
-echo "starting app"
-cd ../bin
-./ia
+
+###############################  AASDK #########################
+if [ $aasdk = false ]; then
+	echo -e Skipping aasdk '\n'
+else
+  #change to parent directory
+  cd ..
+
+  #clone aasdk
+  git clone $aasdkRepo
+  if [[ $? -eq 0 ]]; then
+    echo -e Aasdk Cloned ok '\n'
+  else
+    cd aasdk
+    if [[ $? -eq 0 ]]; then
+      git pull $aasdkRepo
+      echo -e Aasdk Cloned OK '\n'
+      cd ..
+    else
+      echo Aasdk clone/pull error
+      exit
+    fi
+  fi
+
+  #change into aasdk folder
+  echo -e moving to aasdk '\n'
+  cd aasdk
+
+  echo -e Making AASDK build directory '\n'
+
+  mkdir build
+  if [[ $? -eq 0 ]]; then
+    echo -e AASDK build directory made
+  else
+    echo Unable to create AASDK build directory assuming it exists...
+  fi
+
+  cd build
+
+  #beginning cmake
+  cmake -DCMAKE_BUILD_TYPE=Release ../
+  if [[ $? -eq 0 ]]; then
+      echo -e Aasdk CMake completed successfully'\n'
+  else
+    echo Aasdk CMake failed with code $?
+    exit
+  fi
+
+  #beginning make
+  make -j2
+
+  if [[ $? -eq 0 ]]; then
+    echo -e Aasdk Make completed successfully '\n'
+  else
+    echo Aasdk Make failed with code $?
+    exit
+  fi
+
+  #begin make install
+  sudo make install
+
+  if [[ $? -eq 0 ]]
+    then
+    echo -e Aasdk installed ok'\n'
+    echo
+  else
+    echo Aasdk install failed with code $?
+    exit
+  fi
+  cd ../dash
+fi
+
+
+###############################  gstreamer  #########################
+#check if gstreamer install is requested
+if [ $gstreamer = true ]; then
+  echo installing gstreamer
+
+  #change to parent directory
+  cd ..
+
+  #clone gstreamer
+  echo Cloning Gstreamer
+  git clone $gstreamerRepo
+  if [[ $? -eq 0 ]]; then
+    echo -e Gstreamer cloned OK
+  else
+    cd qt-gstreamer
+      if [[ $? -eq 0 ]]; then
+        git pull $gstreamerRepo
+        echo -e cloned OK '\n'
+        cd ..
+      else
+          echo Gstreamer clone/pull error
+        exit
+      fi
+  fi
+
+  #change into newly cloned directory
+  cd qt-gstreamer
+
+  #create build directory
+  echo creating Gstreamer build directory
+  mkdir build
+
+  if [[ $? -eq 0 ]]; then
+    echo -e Gstreamer build directory made
+  else
+    echo Unable to create Gstreamer build directory assuming it exists...
+  fi
+
+  cd build
+
+  #run cmake
+  echo Beginning cmake
+  cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH) -DCMAKE_INSTALL_INCLUDEDIR=include -DQT_VERSION=5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-std=c++11
+
+  if [[ $? -eq 0 ]]; then
+    echo -e Make ok'\n'
+  else
+    echo Gstreamer CMake failed
+    exit
+  fi
+
+  echo Making Gstreamer
+  make -j4
+
+  if [[ $? -eq 0 ]]; then
+    echo -e Gstreamer make ok'\n'
+  else
+    echo Make failed with error code $?
+    exit
+  fi
+
+  #run make install
+  echo Beginning make install
+  sudo make install
+
+  if [[ $? -eq 0 ]]; then
+    echo -e Gstreamer installed ok'\n'
+  else
+    echo Gstreamer make install failed with error code $?
+    exit
+  fi
+
+  sudo ldconfig
+  cd ../../dash
+
+else
+	echo -e Skipping Gstreamer'\n'
+fi
+
+
+
+###############################  openauto  #########################
+if [ $openauto = false ]; then
+  echo -e skipping openauto'\n'
+else
+  echo Installing openauto
+  cd ..
+
+  echo -e cloning openauto'\n'
+  git clone $openautoRepo
+  if [[ $? -eq 0 ]]; then
+    echo -e cloned OK'\n'
+  else
+    cd openauto
+    if [[ $? -eq 0 ]]; then
+      git pull $openautoRepo
+      echo -e Openauto cloned OK'\n'
+      cd ..
+    else
+      echo Openauto clone/pull error
+      exit
+    fi
+  fi
+
+  cd openauto
+
+  echo -e Making openauto build directory '\n'
+
+  mkdir build
+  if [[ $? -eq 0 ]]; then
+    echo -e openauto build directory made
+  else
+    echo Unable to create openauto build directory assuming it exists...
+  fi
+
+  cd build
+
+  echo Beginning openauto cmake
+  cmake ${installArgs} -DGST_BUILD=true ../
+  if [[ $? -eq 0 ]]; then
+    echo -e Openauto CMake OK'\n'
+  else
+    echo Openauto CMake failed with error code $?
+    exit
+  fi
+
+  echo Beginning openauto make
+  make
+  if [[ $? -eq 0 ]]; then
+    echo -e Openauto make OK'\n'
+  else
+    echo Openauto make failed with error code $?
+    exit
+  fi
+
+  #run make install
+  echo Beginning make install
+  sudo make install
+  if [[ $? -eq 0 ]]; then
+    echo -e Openauto installed ok'\n'
+  else
+    echo Openauto make install failed with error code $?
+    exit
+  fi
+  cd ../dash
+fi
+
+
+###############################  dash  #########################
+if [ $dash = false ]; then
+	echo -e Skipping dash'\n'
+else
+
+  mkdir build
+  if [[ $? -eq 0 ]]; then
+    echo -e dash build directory made
+  else
+    echo Unable to create dash build directory assuming it exists...
+  fi
+
+  cd build
+
+	echo -e Installing dash'\n'
+  echo Running CMake for dash
+  cmake ${installArgs} -DGST_BUILD=TRUE ../
+  if [[ $? -eq 0 ]]; then
+    echo -e Dash CMake OK'\n'
+  else
+    echo Dash CMake failed with error code $?
+    exit
+  fi
+
+  echo Running Dash make
+  make
+  if [[ $? -eq 0 ]]; then
+      echo -e Dash make ok, executable can be found ../bin/ia
+      echo
+
+      #check and add usb rules for openauto if they dont exist
+      echo Checking if permissions exist
+      #udev rule to be created below, change as needed
+      FILE=/etc/udev/rules.d/51-iadash.rules
+      if [[ ! -f "$FILE" ]]; then
+          # OPEN USB RULE, CREATE MORE SECURE RULE IF REQUIRED
+          echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"*\", ATTR{idProduct}==\"*\", MODE=\"0660\", GROUP=\"plugdev\"" | sudo tee $FILE
+        if [[ $? -eq 0 ]]; then
+            echo -e Permissions created'\n'
+          else
+            echo -e Unable to create permissions'\n'
+        fi
+        else
+          echo -e Rules exists'\n'
+      fi
+    else
+      echo Dash make failed with error code $?
+      exit
+  fi
+
+  #Setting openGL driver and GPU memory to 256mb
+  if $isRpi; then
+    sudo raspi-config nonint do_memory_split 256
+    if [[ $? -eq 0 ]]; then
+      echo -e Memory set to 256mb'\n'
+    else
+      echo Setting memory failed with error code $? please set manually
+      exit
+    fi
+
+    sudo raspi-config nonint do_gldriver G2
+    if [[ $? -eq 0 ]]; then
+      echo -e OpenGL set ok'\n'
+    else
+      echo Setting openGL failed with error code $? please set manually
+      exit
+    fi
+
+  fi
+
+
+  #Start app
+  echo Starting app
+  cd ../bin
+  ./ia
+fi
