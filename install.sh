609f81ece81ce836ea2b61e1e59c307a4d870386
Danny Barake <danny@1tis.nl>
2020-08-12 13:15:37 +0200

    Added desktop icon to start opendash

--- install.sh
+++ install.sh
@@ -101,6 +101,28 @@ if [[ $? > 0 ]]
     fi
 fi
 
+#remove existing opendash desktop
+rm /home/pi/Desktop/opendash
+
+#copy icon to pixmaps folder
+sudo cp assets/icons/opendash.xpm /usr/share/pixmaps/opendash.xpm
+
+#copy executable to /usr/local/bin
+sudo cp bin/ia /usr/local/bin/ia
+
+#create shortcut on dashboard
+echo "
+[Desktop Entry]
+Name=OpenDash
+Comment=Open OpenDash
+Icon=/usr/share/pixmaps/opendash.xpm
+Exec=/usr/local/bin/ia
+Type=Application
+Encoding=UTF-8
+Terminal=true
+Categories=None;
+" > /home/pi/Desktop/opendash
+
 #Start app
 echo "starting app"
 cd ../bin
