bdb3918f83d280483d480f0508dc8008bdbd7f8f
egisz <e.zideckas@gmail.com>
2021-08-05 10:52:19 +0300

    fix RPM for Fxx model

--- include/obd/decoders.hpp
+++ include/obd/decoders.hpp
@@ -8,7 +8,7 @@ double temp(Response resp) { return (int)resp.data.at(0) - 48; }
 
 double torque(Response resp) { return ((256.0 * (int)resp.data.at(2)) + (int)resp.data.at(1)) / 32.0; }
 
-double rpm(Response resp) { return ((256.0 * (int)resp.data.at(5)) + (int)resp.data.at(4)) / 4.0; }
+double rpm(Response resp) { return ((256.0 * (int)resp.data.at(6)) + (int)resp.data.at(5)) / 4.0; }
 
 double speed(Response resp) { return ((256.0 * (int)resp.data.at(3)) + (int)resp.data.at(2)) / 100.0; }
 
--- src/obd/command.cpp
+++ src/obd/command.cpp
@@ -3,7 +3,7 @@
 
 Commands cmds = {{"Torque", QCanBusFrame(0xa8, QByteArray::fromHex("0201040000000000")), torque},
                  {"Engine Coolant Temperature", QCanBusFrame(0x1d0, QByteArray::fromHex("0201050000000000")), temp},
-                 {"Engine Revolutions Per Minute (RPM)", QCanBusFrame(0xaa, QByteArray::fromHex("02010C0000000000")), rpm},
+                 {"Engine Revolutions Per Minute (RPM)", QCanBusFrame(0x0a5, QByteArray::fromHex("02010C0000000000")), rpm},
                  {"Vehicle Speed", QCanBusFrame(0x1a1, QByteArray::fromHex("02010D0000000000")), speed},
                  {"Intake Air Temperature", QCanBusFrame(0x7df, QByteArray::fromHex("02010F0000000000")), temp},
                  {"Mass Air Flow (MAF) Rate", QCanBusFrame(0x7df, QByteArray::fromHex("0201100000000000")), flow}};
