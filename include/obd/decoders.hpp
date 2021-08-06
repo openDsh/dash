c7a7865ae18ec382b0ce60b121e9f931dc960e09
egisz <e.zideckas@gmail.com>
2021-08-06 11:01:08 +0300

    revert decoders file, it should go to plugin anyway

--- include/obd/decoders.hpp
+++ include/obd/decoders.hpp
@@ -4,13 +4,11 @@
 
 double percentage(Response resp) { return (100.0 / 255.0) * (int)resp.data.at(0); }
 
-double temp(Response resp) { return (int)resp.data.at(0) - 48; }
+double temp(Response resp) { return (int)resp.data.at(0) - 40; }
 
-double torque(Response resp) { return ((256.0 * (int)resp.data.at(2)) + (int)resp.data.at(1)) / 32.0; }
+double rpm(Response resp) { return ((256.0 * (int)resp.data.at(0)) + (int)resp.data.at(1)) / 4.0; }
 
-double rpm(Response resp) { return ((256.0 * (int)resp.data.at(6)) + (int)resp.data.at(5)) / 4.0; }
-
-double speed(Response resp) { return ((256.0 * (int)resp.data.at(3)) + (int)resp.data.at(2)) / 100.0; }
+double speed(Response resp) { return (int)resp.data.at(0); }
 
 double flow(Response resp) { return ((256.0 * (int)resp.data.at(0)) + (int)resp.data.at(1)) / 100.0; }
 
