ba60c208458a648685e2de919bb74fb0f7e7fb65
Gioele <gioekant@gmail.com>
2023-04-16 02:53:00 +0200

    a

--- include/canbus/socketcanbus.hpp
+++ include/canbus/socketcanbus.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
-#include <QCanBus> 
+#include <QCanBus>
 #include <string>
 #include <QVector>
 #include <QByteArray>
 #include <QVariant>
 #include <QTcpSocket>
+#include <QAbstractSocket>
 #include "DashLog.hpp"
 #include "canbus/ICANBus.hpp"
 #include "app/config.hpp"
@@ -13,16 +14,20 @@
 class SocketCANBus : public ICANBus
 {
     Q_OBJECT
-    public:
-        SocketCANBus(QString canInterface = "can0");
-        ~SocketCANBus();
-        static SocketCANBus *get_instance();
-        bool writeFrame(QString frame) override;
+public:
+    SocketCANBus(QString canInterface = "can0");
+    ~SocketCANBus();
+    static SocketCANBus *get_instance();
+    bool writeFrame(QString frame) override;
 
-    private:
-        bool socketCANAvailable = false;
-        QTcpSocket socket;
-        QCanBusDevice *bus;
-        std::map<int, std::vector<std::function<void(QByteArray)>>> callbacks;
+private:
+    bool socketCANAvailable = false;
+    QTcpSocket socket;
+    QCanBusDevice *bus;
+    std::map<int, std::vector<std::function<void(QByteArray)>>> callbacks;
+
+signals:
+
+public slots:
+    void readyRead();
 };
-
--- src/canbus/socketcanbus.cpp
+++ src/canbus/socketcanbus.cpp
@@ -37,138 +37,7 @@ SocketCANBus::SocketCANBus(QString canInterface)
             DASH_LOG(error) << "[SocketCANBus] Errore di connessione a Carberry";
         }
 
-        int lumws = 10;
-
-        while (this->socket.canReadLine())
-        {
-
-            DASH_LOG(info) << "[SocketCANBus] Comincio a leggere i CANBUS";
-
-            QString linea = QString(this->socket.readLine());
-            DASH_LOG(info) << linea.toStdString();
-            QStringList part = linea.split(" ");
-            QStringList fram = part[1].split("-");
-
-            QStringList hexx;
-            int index = 0;
-
-            for (int i = 0; i < fram[1].length(); i++)
-            {
-                if (i % 2 == 0)
-                {
-                    hexx[index] = fram[1].mid(i - 2, 2);
-                    index++;
-                }
-            }
-
-            // MS-CAN
-
-            if (part.at(0) == "RX1")
-            {
-
-                // Esempio RX1 0206-008401
-
-                if (fram.at(0) == "0206")
-                {
-                    if (hexx.at(0) == "00")
-                    {
-                        bool ok;
-
-                        uint temprem = hexx[2].toUInt(&ok, 16);
-
-                        if (ok == true && temprem > 64)
-                        {
-                            if (hexx.at(1) == "81")
-                                DASH_LOG(info) << "PREMUTO pulsante in alto a sinistra\r\n";
-                            if (hexx.at(1) == "82")
-                                DASH_LOG(info) << "PREMUTO Pulsante giù a sinistra\r\n";
-                            if (hexx.at(1) == "84")
-                                DASH_LOG(info) << "PREMUTO Pulsante manopola sinistra\r\n";
-                            if (hexx.at(1) == "91")
-                                DASH_LOG(info) << "PREMUTO Pulsante destro in alto (successivo)\r\n";
-                            if (hexx.at(1) == "92")
-                                DASH_LOG(info) << "PREMUTO Pulsante in basso a destra\r\n";
-                        }
-                        else
-                        {
-                            if (hexx.at(1) == "81")
-                                DASH_LOG(info) << "pulsante in alto a sinistra\r\n";
-                            if (hexx.at(1) == "82")
-                                DASH_LOG(info) << "Pulsante giù a sinistra\r\n";
-                            if (hexx.at(1) == "84")
-                                DASH_LOG(info) << "Pulsante manopola sinistra\r\n";
-                            if (hexx.at(1) == "91")
-                                DASH_LOG(info) << "Pulsante destro in alto (successivo)\r\n";
-                            if (hexx.at(1) == "92")
-                                DASH_LOG(info) << "Pulsante in basso a destra\r\n";
-                        }
-                    }
-                    if (hexx.at(0) == "08")
-                    {
-                        if (hexx.at(1) == "83")
-                        {
-                            if (hexx.at(2).at(1) == "F")
-                            {
-                                DASH_LOG(info) << "Manopola sinistra SU\r\n";
-                            }
-                            else
-                            {
-                                DASH_LOG(info) << "Manopola sinistra GIU\r\n";
-                            }
-                        }
-
-                        if (hexx.at(1) == "93")
-                        {
-                            if (hexx.at(2).at(1) == "F")
-                            {
-                                DASH_LOG(info) << "Manopola destra (Volume) GIU\r\n";
-                            }
-                            else
-                            {
-                                DASH_LOG(info) << "Manopola destra (Volume) SU\r\n";
-                            }
-                        }
-                    }
-                }
-
-                // Esempio RX1 0450-460706FF
-
-                if (fram.at(0) == "0450")
-                {
-                    bool okk;
-
-                    uint luminosita_nuova = hexx[3].toUInt(&okk, 16);
-                    int valore_ws = (int)(luminosita_nuova / 25.5);
-
-                    if (valore_ws != lumws)
-                    {
-                        /*
-                        char query[73]; // 71
-
-                        sprintf(query, "/home/gioele/RPi-USB-Brightness/64/lite/Raspi_USB_Backlight_nogui -b %d", valore_ws);
-
-                        if (system(query) == 0)
-                        {
-                            printf("%s\r\n", query);
-                        }
-                        else
-                        {
-                            printf("==== ERRORE LUMINOSITA ====\r\n");
-                        }*/
-
-                        DASH_LOG(info) << "NUOVA LUMINOSITA:" << QString::number(valore_ws).toStdString()<< "\r\n";
-
-                        lumws = valore_ws;
-                    }
-                }
-            }
-
-            // HS-CAN
-
-            if (part.at(0) == "RX2")
-            {
-            }
-        }
+        connect(socket, SIGNAL(readyRead()), this, SLOT(readyRead()));
     }
 }
 
@@ -190,4 +59,141 @@ SocketCANBus *SocketCANBus::get_instance()
 {
     static SocketCANBus bus(Config::get_instance()->get_vehicle_interface());
     return &bus;
+}
+
+void SocketCANBus::readyRead()
+{
+
+    int lumws = 10;
+
+    while (this->socket.canReadLine())
+    {
+
+        DASH_LOG(info) << "[SocketCANBus] Comincio a leggere i CANBUS";
+
+        QString linea = QString(this->socket.readLine());
+        DASH_LOG(info) << linea.toStdString();
+        QStringList part = linea.split(" ");
+        QStringList fram = part[1].split("-");
+
+        QStringList hexx;
+        int index = 0;
+
+        for (int i = 0; i < fram[1].length(); i++)
+        {
+            if (i % 2 == 0)
+            {
+                hexx[index] = fram[1].mid(i - 2, 2);
+                index++;
+            }
+        }
+
+        // MS-CAN
+
+        if (part.at(0) == "RX1")
+        {
+
+            // Esempio RX1 0206-008401
+
+            if (fram.at(0) == "0206")
+            {
+                if (hexx.at(0) == "00")
+                {
+                    bool ok;
+
+                    uint temprem = hexx[2].toUInt(&ok, 16);
+
+                    if (ok == true && temprem > 64)
+                    {
+                        if (hexx.at(1) == "81")
+                            DASH_LOG(info) << "PREMUTO pulsante in alto a sinistra\r\n";
+                        if (hexx.at(1) == "82")
+                            DASH_LOG(info) << "PREMUTO Pulsante giù a sinistra\r\n";
+                        if (hexx.at(1) == "84")
+                            DASH_LOG(info) << "PREMUTO Pulsante manopola sinistra\r\n";
+                        if (hexx.at(1) == "91")
+                            DASH_LOG(info) << "PREMUTO Pulsante destro in alto (successivo)\r\n";
+                        if (hexx.at(1) == "92")
+                            DASH_LOG(info) << "PREMUTO Pulsante in basso a destra\r\n";
+                    }
+                    else
+                    {
+                        if (hexx.at(1) == "81")
+                            DASH_LOG(info) << "pulsante in alto a sinistra\r\n";
+                        if (hexx.at(1) == "82")
+                            DASH_LOG(info) << "Pulsante giù a sinistra\r\n";
+                        if (hexx.at(1) == "84")
+                            DASH_LOG(info) << "Pulsante manopola sinistra\r\n";
+                        if (hexx.at(1) == "91")
+                            DASH_LOG(info) << "Pulsante destro in alto (successivo)\r\n";
+                        if (hexx.at(1) == "92")
+                            DASH_LOG(info) << "Pulsante in basso a destra\r\n";
+                    }
+                }
+                if (hexx.at(0) == "08")
+                {
+                    if (hexx.at(1) == "83")
+                    {
+                        if (hexx.at(2).at(1) == "F")
+                        {
+                            DASH_LOG(info) << "Manopola sinistra SU\r\n";
+                        }
+                        else
+                        {
+                            DASH_LOG(info) << "Manopola sinistra GIU\r\n";
+                        }
+                    }
+
+                    if (hexx.at(1) == "93")
+                    {
+                        if (hexx.at(2).at(1) == "F")
+                        {
+                            DASH_LOG(info) << "Manopola destra (Volume) GIU\r\n";
+                        }
+                        else
+                        {
+                            DASH_LOG(info) << "Manopola destra (Volume) SU\r\n";
+                        }
+                    }
+                }
+            }
+
+            // Esempio RX1 0450-460706FF
+
+            if (fram.at(0) == "0450")
+            {
+                bool okk;
+
+                uint luminosita_nuova = hexx[3].toUInt(&okk, 16);
+                int valore_ws = (int)(luminosita_nuova / 25.5);
+
+                if (valore_ws != lumws)
+                {
+                    /*
+                    char query[73]; // 71
+
+                    sprintf(query, "/home/gioele/RPi-USB-Brightness/64/lite/Raspi_USB_Backlight_nogui -b %d", valore_ws);
+
+                    if (system(query) == 0)
+                    {
+                        printf("%s\r\n", query);
+                    }
+                    else
+                    {
+                        printf("==== ERRORE LUMINOSITA ====\r\n");
+                    }*/
+
+                    DASH_LOG(info) << "NUOVA LUMINOSITA:" << QString::number(valore_ws).toStdString() << "\r\n";
+
+                    lumws = valore_ws;
+                }
+            }
+        }
+
+        // HS-CAN
+
+        if (part.at(0) == "RX2")
+        {
+        }
+    }
 }
