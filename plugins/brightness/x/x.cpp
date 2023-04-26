49fe159522803eda5949e025094d63fbf4493916
Gioele <gioekant@gmail.com>
2023-04-26 18:50:42 +0200

    prova

--- plugins/brightness/x/x.cpp
+++ plugins/brightness/x/x.cpp
@@ -25,7 +25,7 @@ void X::set(int brightness)
     if (this->screen != nullptr) {
         QProcess process(this);
         QString stampa = QStringLiteral("[LUM] BARRA = %1 RISULTATO %2 \n").arg(brightness).arg((int)(brightness / 25.5));
-        qDebug() << stampa.toStdString();
+        //qDebug() << stampa.toStdString();
         process.start(QString("/home/gioele/RPi-USB-Brightness/64/lite/Raspi_USB_Backlight_nogui -b %1").arg((int)(brightness / 25.5)));
         process.waitForFinished();
     }
