dad6c6ef6d9380543f290a6440611cae2d74c6ca
Gioele <gioekant@gmail.com>
2023-04-26 16:06:56 +0200

    prova

--- plugins/vehicle/test/test.cpp
+++ plugins/vehicle/test/test.cpp
@@ -147,7 +147,7 @@ bool Test::init(SocketCANBus *bus)
             } });
         timer2->start(100000);
 
-        QObject::connect(&bus->socket, &QTcpSocket::readyRead, this, &Test::readFrame(bus));
+        QObject::connect(&bus->socket, &QTcpSocket::readyRead, this, &Test::readFrame(*bus));
 
         return true;
     }
