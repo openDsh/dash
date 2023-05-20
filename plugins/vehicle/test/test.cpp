271c2fb67423823b0e5a8ac218d969056af1fa1c
Gioele <gioekant@gmail.com>
2023-05-20 19:11:45 +0200

    prova

--- plugins/vehicle/test/test.cpp
+++ plugins/vehicle/test/test.cpp
@@ -12,7 +12,7 @@ Test::~Test()
     if (this->vehicle)
         delete this->vehicle;
     //if (this->obd)
-      //  delete this->obd;
+        //delete this->obd;
 }
 
 QList<QWidget *> Test::widgets()
@@ -20,8 +20,8 @@ QList<QWidget *> Test::widgets()
     QList<QWidget *> tabs;
     if (this->vehicle)
         tabs.append(this->vehicle);
-    if (this->obd)
-        tabs.append(this->obd);
+    //if (this->obd)
+        //tabs.append(this->obd);
     return tabs;
 }
 
