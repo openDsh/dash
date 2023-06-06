18622fe5bbc5204b2f511af972be912083b1fd02
Cosmin Tanislav <demonsingur@gmail.com>
2023-06-06 15:31:22 +0300

    Make use of all available threads when installing

--- install.sh
+++ install.sh
@@ -204,7 +204,7 @@ if [ $pulseaudio = false ]
     sed -i 's/*imtu = 48;/*imtu = 60;/g' src/modules/bluetooth/backend-ofono.c
     sudo apt-get build-dep -y pulseaudio
     ./bootstrap.sh
-    make -j4
+    make -j$(nproc)
     sudo make install
     sudo ldconfig
     # copy configs and force an exit 0 just in case files are identical (we don't care but it will make pimod exit)
@@ -249,7 +249,7 @@ if [ $bluez = false ]
     rm bluez-5.63.tar.xz
     cd bluez-5.63
     ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --enable-library --disable-manpages --enable-deprecated
-    make
+    make -j$(nproc)
     sudo make install
     cd ..
 fi
@@ -303,7 +303,7 @@ else
   fi
 
   #beginning make
-  make -j2
+  make -j$(nproc)
 
   if [[ $? -eq 0 ]]; then
     echo -e Aasdk Make completed successfully '\n'
@@ -374,7 +374,7 @@ else
   fi
 
   #beginning make
-  make
+  make -j$(nproc)
 
   if [[ $? -eq 0 ]]; then
     echo -e h264bitstream Make completed successfully '\n'
@@ -459,7 +459,7 @@ if [ $gstreamer = true ]; then
   fi
 
   echo Making Gstreamer
-  make -j4
+  make -j$(nproc)
 
   if [[ $? -eq 0 ]]; then
     echo -e Gstreamer make ok'\n'
@@ -535,7 +535,7 @@ else
   fi
 
   echo Beginning openauto make
-  make
+  make -j$(nproc)
   if [[ $? -eq 0 ]]; then
     echo -e Openauto make OK'\n'
   else
@@ -584,7 +584,7 @@ else
   fi
 
   echo Running Dash make
-  make
+  make -j$(nproc)
   if [[ $? -eq 0 ]]; then
       echo -e Dash make ok, executable can be found ../bin/dash
       echo
